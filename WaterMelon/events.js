4dff8a7b2cc1abad29f786cdab6bf398b60ee0ff
ezstrachan24 <38891978+ezstrachan24@users.noreply.github.com>
2018-08-23 08:45:03 -0400

    Second Update

---
+++ WaterMelon/events.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var dayEvent = new Array();
+
+dayEvent[1] = "";
+dayEvent[2] = "<br /><a href='#'>The WaterMelon Show</a><br />7 pm <br />";
+dayEvent[3] = "<br /><a href='#'>The WaterMelon Show</a><br />8 pm<br />";
+dayEvent[4] = "<br /><a href='#'>WaterMelon After Dark</a><br />7:30 pm<br />";
+dayEvent[5] = "<br /><a href='#'>WaterMelon and Me</a><br />11 am<br />";
+dayEvent[6] = "<br /><a href='#'>Love  and WaterMelon</a><br />7 pm<br />";
+dayEvent[7] = "";
+
+
+dayEvent[8] = "<br /><a href='#'>WaterMelon Family and Friends</a><br />10 am<br />";
+dayEvent[9] = "<br /><a href='#'>The WaterMelon Show</a><br />7:30 pm<br />";
+dayEvent[10] = "";
+dayEvent[11] = "<br /><a href='#'>The WaterMelon Show</a><br />7:30 pm<br />";
+dayEvent[12] = "";
+dayEvent[13] = "";
+dayEvent[14] = "<br /><a href='#'>Wine and WaterMelon</a><br />7 pm<br />";
+
+dayEvent[15] = "<br /><a href='#'>WaterMelon Family and Friends</a><br />10:30 am<br />";
+dayEvent[16] = "<br /><a href='#'>Love  and WaterMelon</a><br />7:30 pm<br />";
+dayEvent[17] = "";
+dayEvent[18] = "";
+dayEvent[19] = "<br /><a href='#'>WaterMelon Family and Friends</a><br />11 am<br />";
+dayEvent[20] = "<br /><a href='#'>Love  and WaterMelon</a><br />7 pm <br />";
+dayEvent[21] = "";
+
+dayEvent[22] = "";
+dayEvent[23] = "<br /><a href='#'>Wine and WaterMelon</a><br />7 pm<br />";
+dayEvent[24] = "";
+dayEvent[25] = "<br /><a href='#'>WaterMelon After Dark</a><br />8 pm<br />";
+dayEvent[26] = "<br /><a href='#'>WaterMelon Gallery Tour</a><br />11 am<br />";
+dayEvent[27] = "";
+dayEvent[28] = "<br /><a href='#'>WaterMelon Gallery Show </a><br />6 pm<br />";
+
+dayEvent[29] = "<br /><a href='#'>WaterMelon Fair</a><br />11 am<br />";
+dayEvent[30] = "";
+dayEvent[31] = "<br /><a href='#'>The WaterMelon Show</a><br />7:30 pm<br />";
